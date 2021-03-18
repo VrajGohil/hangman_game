bad885bbe9c4e6a4d02f72752e92834f0956db66
Vraj Gohil <vrajhil3999@gmail.com>
2021-03-18 06:34:34 +0000

    init

---
+++ script.js
@@ -0,0 +1,120 @@
+const wordElement = document.getElementById("word");
+const questionElement = document.getElementById("question");
+const wrongLettersElement = document.getElementById("wrong-letters");
+const playAgainButton = document.getElementById("play-button");
+const popup = document.getElementById("popup-container");
+const notification = document.getElementById("notification-container");
+const finalMessage = document.getElementById("final-message");
+const finalMessageRevealWord = document.getElementById(
+  "final-message-reveal-word"
+);
+const figureParts = document.querySelectorAll(".figure-part");
+const questions = [
+  "What is the SI Unit of electric current?",
+  "A device that is used to break an electric circuit is called a",
+  "What is the SI unit of electric charge?",
+  "Which device is used for measuring electric current?",
+  "An ammeter is attached to the circuit in which combination?",
+];
+const words = [
+  "ampere",
+  "switch",
+  "coulomb",
+  "ammeter",
+  "series"
+];
+let randonNumber = Math.floor(Math.random() * words.length);
+let selectedWord = words[randonNumber];
+let selectedQuestion = questions[randonNumber];
+
+let playable = true;
+
+const correctLetters = [];
+const wrongLetters = [];
+
+function displayWord() {
+  wordElement.innerHTML = `
+    ${selectedWord
+      .split("") // to array
+      .map(
+        (letter) => `
+    <span class="letter">
+    ${correctLetters.includes(letter) ? letter : ""}
+    </span>
+    `
+      )
+      .join("")} 
+    `; // to string
+  questionElement.innerText = selectedQuestion;
+  const innerWord = wordElement.innerText.replace(/\n/g, "");
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = "Congratulations! You won! 😃";
+    finalMessageRevealWord.innerText = "";
+    popup.style.display = "flex";
+    playable = false;
+  }
+}
+
+function updateWrongLettersElement() {
+  wrongLettersElement.innerHTML = `
+  ${wrongLetters.length > 0 ? "<p>Wrong</p>" : ""}
+  ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
+  `;
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+    index < errors
+      ? (part.style.display = "block")
+      : (part.style.display = "none");
+  });
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = "Unfortunately you lost. 😕";
+    finalMessageRevealWord.innerText = `...the correct answer is: ${selectedWord}`;
+    popup.style.display = "flex";
+    playable = false;
+  }
+}
+
+function showNotification() {
+  notification.classList.add("show");
+  setTimeout(() => {
+    notification.classList.remove("show");
+  }, 2000);
+}
+
+window.addEventListener("keypress", (e) => {
+  if (playable) {
+    const letter = e.key.toLowerCase();
+    if (letter >= "a" && letter <= "z") {
+      if (selectedWord.includes(letter)) {
+        if (!correctLetters.includes(letter)) {
+          correctLetters.push(letter);
+          displayWord();
+        } else {
+          showNotification();
+        }
+      } else {
+        if (!wrongLetters.includes(letter)) {
+          wrongLetters.push(letter);
+          updateWrongLettersElement();
+        } else {
+          showNotification();
+        }
+      }
+    }
+  }
+});
+
+playAgainButton.addEventListener("click", () => {
+  playable = true;
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+  let randonNumber = Math.floor(Math.random() * words.length);
+  selectedWord = words[randonNumber];
+  selectedQuestion = questions[randonNumber];
+  displayWord();
+  updateWrongLettersElement();
+  popup.style.display = "none";
+});
+
+// Init
+displayWord();
